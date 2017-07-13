08338247c2e1fb8cf4317a0604d299e23b3a425c
sacherjj <sacherjj@gmail.com>
2017-07-13 19:47:44 -0400

    Decent Algorithm, but not optimal yet.

--- 20170630_challenge_321_hard_circle_splitter/circle_spliter/src/main.rs
+++ 20170630_challenge_321_hard_circle_splitter/circle_spliter/src/main.rs
@@ -1,26 +1,70 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::io::BufRead;
+use std::str::FromStr;
+use std::io::Read;
+use std::f32;
 
-const BOX_MIN: f64 = 0.0;
-const BOX_MAX: f64 = 1.0;
+const BOX_MIN: f32 = 0.0;
+const BOX_MAX: f32 = 1.0;
 
 #[derive(Debug)]
 struct Point {
-    x: f64,
-    y: f64,
+    x: f32,
+    y: f32,
 }
 
 #[derive(Debug)]
 struct Circle {
     center: Point,
-    r: f64,
+    r: f32,
+}
+
+impl FromStr for Point {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut coords = s.split(' ');
+        let x = coords.next().unwrap().parse().unwrap();
+        let y = coords.next().unwrap().parse().unwrap();
+        match coords.next() {
+            None    => Ok(Point { x, y }),
+            Some(_) => Err(()),
+        }
+    }
+}
+
+
+pub fn nearly_equal(a: f32, b: f32) -> bool {
+	let abs_a = a.abs();
+	let abs_b = b.abs();
+	let diff = (a - b).abs();
+
+	if a == b { // Handle infinities.
+		true
+	} else if a == 0.0 || b == 0.0 || diff < f32::MIN_POSITIVE {
+		// One of a or b is zero (or both are extremely close to it,) use absolute error.
+		diff < (f32::EPSILON * f32::MIN_POSITIVE)
+	} else { // Use relative error.
+		(diff / f32::min(abs_a + abs_b, f32::MAX)) < f32::EPSILON*1.1
+	}
+}
+
+pub fn float_less(a: f32, b: f32) -> bool {
+    if nearly_equal(a, b) {
+        false
+    } else {
+        a < b
+    }
 }
 
 impl Circle {
 
-    pub fn new(center: Point, radius: f64) -> Circle {
+    pub fn new(center: Point, radius: f32) -> Circle {
         Circle{center: center, r: radius}
+    }
+
+    pub fn update(&mut self, cir: &Circle) {
+        self.center.x = cir.center.x;
+        self.center.y = cir.center.y;
+        self.r = cir.r;
     }
 
     /// Returns a Circle that passes through the three points.
@@ -54,20 +98,22 @@ impl Circle {
         let c_x = (j.x - k.x)/2. + k.x;
         let c_y = (j.y - k.y)/2. + k.y;
         let r = ((j.x - k.x)*(j.x - k.x) + (j.y - k.y)*(j.y - k.y)).sqrt() / 2.;
-        Circle{center: Point{x: c_x, y: c_y}, r: r}
+        Circle::new(Point{x: c_x, y: c_y}, r)
     }
 
     /// Check if the circle is contained in a square from 0.0 to 1.0 (x and y)
     pub fn in_square(&self) -> bool {
         let r = self.r;
-        if r > (BOX_MAX - BOX_MIN)/2. {
+        if float_less((BOX_MAX - BOX_MIN)/2., r) {
             return false;
         }
         let Point { x, y } = self.center;
-        if (x - r) < BOX_MIN || (x + r) > BOX_MAX {
+        if float_less(x - r, BOX_MIN) ||
+            float_less(BOX_MAX, x + r) {
             return false;
         }
-        if (y - r) < BOX_MIN || (y + r) > BOX_MAX {
+        if float_less(y - r, BOX_MIN) ||
+            float_less(BOX_MAX, y + r) {
             return false;
         }
         true
@@ -75,43 +121,128 @@ impl Circle {
 
     /// Check if point is inside circle
     /// Using pythagorean, but squaring radius instead of sqrt.
-    pub fn point_inside(&self, p: &Point) -> bool {
+    pub fn is_point_inside(&self, p: &Point) -> bool {
         let ref c = self.center;
         let d_sq = (p.x - c.x)*(p.x - c.x) + (p.y - c.y)*(p.y - c.y);
-        d_sq < self.r*self.r
+        let r_sq = self.r*self.r;
+//        println!("{} {}", r_sq, d_sq);
+        if nearly_equal(d_sq, r_sq) {
+//            println!("Nearly Equal");
+            true
+        } else {
+            d_sq < r_sq
+        }
+    }
+
+    /// Counts how many points are inside circle
+    pub fn points_inside_count(&self, points: &Vec<Point>) -> i32 {
+        let mut count: i32 = 0;
+        for point in points {
+            if self.is_point_inside(point) {
+                count += 1;
+            }
+        }
+        count
+    }
+}
+
+fn calc_smallest_circle(test_points: &Vec<Point>) -> Option<Circle> {
+    let length = test_points.len();
+    let target: i32 = length as i32 / 2;
+    let mut found = false;
+    let mut smallest = Circle::new(Point{x: 0.5, y:0.5}, 1.0);
+    // Test for solution with 2 points
+    for pta_ind in 0..length - 1 {
+        for ptb_ind in pta_ind + 1..length {
+            let pta = &test_points[pta_ind];
+            let ptb = &test_points[ptb_ind];
+            //            println!("{:?} and {:?}", pta, ptb);
+            let cir = Circle::from_two_points(pta, ptb);
+            //            println!("Trying {:?}", cir);
+            if !cir.in_square() {
+                //                println!("Not in square");
+                continue;
+            }
+            let points_in = cir.points_inside_count(&test_points);
+            //            println!("Points in {}", points_in);
+            if points_in == target {
+                if found {
+                    if cir.r < smallest.r {
+                        smallest.update(&cir);
+                    }
+                } else {
+                    found = true;
+                    smallest.update(&cir);
+                }
+            }
+        }
+    }
+    for pta_ind in 0..length - 2 {
+        for ptb_ind in pta_ind + 1..length-1 {
+            for ptc_ind in ptb_ind + 1..length {
+                let pta = &test_points[pta_ind];
+                let ptb = &test_points[ptb_ind];
+                let ptc = &test_points[ptc_ind];
+                //            println!("{:?} and {:?}", pta, ptb);
+                let cir = Circle::from_three_points(pta, ptb, ptc);
+                //            println!("Trying {:?}", cir);
+                if !cir.in_square() {
+                    //                println!("Not in square");
+                    continue;
+                }
+                let points_in = cir.points_inside_count(&test_points);
+                //            println!("Points in {}", points_in);
+                if points_in == target {
+                    if found {
+                        if cir.r < smallest.r {
+                            smallest.update(&cir);
+                        }
+                    } else {
+                        found = true;
+                        smallest.update(&cir);
+                    }
+                }
+            }
+        }
+    }
+
+    if found {
+        Some(smallest)
+    } else {
+        None
     }
 }
 
 
-fn get_test_data(filename: &str) -> Vec<Vec<f64>> {
-    let file = File::open(filename).unwrap();
-    let reader = BufReader::new(&file);
-    let line_sets: Vec<Vec<f64>> = reader.lines()
-        .filter_map(
-            |l| l.ok().map(
-                |s| s.split_whitespace()
-                    .map(|num| num.parse().unwrap())
-                    .collect()))
-        .collect();
- /*   let mut point_set: Vec<Point> = Vec::new();
-    let mut test_sets: Vec<Vec<Point>> = Vec::new();
-    for set in &line_sets {
-        if set.len() == 1 {
-            if point_set.len() > 0 {
-                test_sets.push(point_set);
-                let mut point_set: Vec<Point> = Vec::new();
-            }
-        } else {
-            point_set.push(Point{x: set[0], y: set[1]})
-        }
-    }*/
-    line_sets
+fn get_test_data(filename: &str) -> Vec<Vec<Point>> {
+    let buf = {
+        let mut buf = String::new();
+        std::fs::File::open(filename).unwrap()
+            .read_to_string(&mut buf).unwrap();
+        buf
+    };
+    let mut lines = buf.lines();
+    let mut point_sets: Vec<Vec<Point>> = Vec::new();
+    while let Some(line) = lines.next() {
+        let n = line.parse::<u32>().unwrap();
+        let points = (0..n).map(|_| lines.next().expect("Point is missing").parse().unwrap()).collect();
+        point_sets.push(points);
+    }
+    point_sets
 }
 
 
 fn main() {
-    let test_sets = get_test_data("../test1.txt");
-    println!("{:?}", test_sets);
+    let test_sets = get_test_data("../test2.txt");
+    for test_set in test_sets {
+        let result = calc_smallest_circle(&test_set);
+        match result {
+            // Solution Found for circle splitter
+            Some(c) => println!("Solution: {:?}", c),
+            // No solution exists
+            None => println!("No Solution")
+        }
+    }
 /*    let a = Point {x: 0.1, y: 0.1};
     let b = Point {x: 0.2, y: 0.2};
     let c = Point {x: 0.3, y: 0.3};
